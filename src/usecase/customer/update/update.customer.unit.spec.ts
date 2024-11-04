1bf5d1f0612ec15f7dc9b68bec9df14c7140686a
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-11-04 20:52:17 -0300

    feat: add update customer dto and unit test to update data

---
+++ src/usecase/customer/update/update.customer.unit.spec.ts
@@ -0,0 +1,37 @@
+import CustomerFactory from "../../../domain/customer/factory/customer.factory";
+import Address from "../../../domain/customer/value-object/address";
+
+const customer = CustomerFactory.createWithAddress(
+  "Higor",
+  new Address("Street", 123, "Zip", "City")
+);
+
+const input = {
+  id: customer.id,
+  name: "Higor Rodrigues",
+  address: {
+    street: "Street Nova",
+    number: 1234,
+    zip: "Zip Novo",
+    city: "City Nova",
+  },
+};
+
+const MockRepository = () => {
+  return {
+    find: jest.fn().mockReturnValue(Promise.resolve(customer)),
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+  };
+};
+
+describe("Unit test customer update use case", () => {
+  it("should update a customer", async () => {
+    const customerRepository = MockRepository();
+    const useCase = new UpdateCustomerUseCase(customerRepository);
+
+    const output = await useCase.execute(input);
+    expect(output).toEqual(input);
+  });
+});
