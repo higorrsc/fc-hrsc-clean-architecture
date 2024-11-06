a2c356cc2d6f6f0a05749718ccd03c3cc6583f92
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-11-05 21:06:24 -0300

    feat: add product find use case with unit and integration tests

---
+++ src/usecase/product/find/find.product.integration.spec.ts
@@ -0,0 +1,43 @@
+import { Sequelize } from "sequelize-typescript";
+import Product from "../../../domain/product/entity/product";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import FindProductUseCase from "./find.product.usecase";
+
+describe("Test find product use case", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should find a product", async () => {
+    const product = new Product("123", "Product 1", 10);
+    const productRepository = new ProductRepository();
+    productRepository.create(product);
+
+    const useCase = new FindProductUseCase(productRepository);
+
+    const input = { id: product.id };
+
+    const output = {
+      id: product.id,
+      name: product.name,
+      price: product.price,
+    };
+
+    const result = await useCase.execute(input);
+    expect(result).toEqual(output);
+  });
+});
