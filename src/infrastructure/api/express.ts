aa84233d4fc6d4ec0222883085499d2a08663e3c
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-11-08 20:28:39 -0300

    feat: add products route and e2e tests

--- src/infrastructure/api/express.ts
+++ src/infrastructure/api/express.ts
@@ -1,12 +1,15 @@
 import express, { Express } from "express";
 import { Sequelize } from "sequelize-typescript";
 import CustomerModel from "../customer/repository/sequelize/customer.model";
+import ProductModel from "../product/repository/sequelize/product.model";
 import { customerRoute } from "./routes/customer.route";
+import { productRoute } from "./routes/products.route";
 
 export const app: Express = express();
 
 app.use(express.json());
 app.use("/customer", customerRoute);
+app.use("/product", productRoute);
 
 export let sequelize: Sequelize;
 
@@ -21,7 +24,7 @@ async function setupDb() {
     CustomerModel,
     // OrderModel,
     // OrderItemModel,
-    // ProductModel,
+    ProductModel,
   ]);
 
   await sequelize.sync();
