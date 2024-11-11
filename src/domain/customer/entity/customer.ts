39f8e7f21caf65493cfa23abe09920d1179757b9
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-11-11 19:48:50 -0300

    feat: extends abstract class entity

--- src/domain/customer/entity/customer.ts
+++ src/domain/customer/entity/customer.ts
@@ -1,20 +1,21 @@
+import Entity from "../../@shared/entity/entity.abstract";
 import Address from "../value-object/address";
 
-export default class Customer {
-  private _id: string;
+export default class Customer extends Entity {
   private _name: string = "";
   private _address!: Address;
   private _active: boolean = false;
   private _rewardPoints: number = 0;
 
   constructor(id: string, name: string) {
-    this._id = id;
+    super();
+    this.id = id;
     this._name = name;
     this.validate();
-  }
 
-  get id(): string {
-    return this._id;
+    if (this.notification.hasErrors()) {
+      throw new Error(this.notification.messages());
+    }
   }
 
   get name(): string {
@@ -34,11 +35,17 @@ export default class Customer {
   }
 
   validate() {
-    if (this._id === "" || this._id.length === 0) {
-      throw new Error("Id is required");
+    if (this.id === "" || this.id.length === 0) {
+      this.notification.addError({
+        context: "customer",
+        message: "Id is required",
+      });
     }
     if (this._name === "" || this._name.length === 0) {
-      throw new Error("Name is required");
+      this.notification.addError({
+        context: "customer",
+        message: "Name is required",
+      });
     }
   }
 
